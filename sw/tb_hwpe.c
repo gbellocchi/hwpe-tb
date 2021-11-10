caedc6fb1dc2c7ee8bac6c6837991298ec37dba1
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2021-11-10 11:48:13 +0100

    modified sw test appliation

--- sw/tb_hwpe.c
+++ sw/tb_hwpe.c
@@ -22,24 +22,75 @@
 #include "archi_hwpe.h"
 #include "hal_hwpe.h"
 
-#include "inc/hwpe_stimuli_a.h"
-#include "inc/hwpe_stimuli_b.h"
-#include "inc/hwpe_stimuli_c.h"
-#include "inc/hwpe_stimuli_d.h"
+// HWPE
+#include "inc/hwpe_lib/archi_hwpe.h"
+#include "inc/hwpe_lib/hal_hwpe.h"
+
+// Synthetic stimuli
+#include "inc/stim/src_V.h"
+
+// Golden results
+#include "inc/stim/dst_V.h"
 
 int main() {
 
-  uint8_t *a = stim_a;
-  uint8_t *b = stim_b;
-  uint8_t *c = stim_c;
-  uint8_t *d = stim_d;
+  // 1. Workload
+  uint32_t width                  = 64; // fill with a value
+  uint32_t height                 = 64; // fill with a value
+  uint32_t stripe_height          = 64; // fill with a value
+  // 2. Custom registers
+  const unsigned width_val = 64; // fill with a value
+  const unsigned height_val = 64; // fill with a value
 
+  /* General parameters. */
   volatile int errors = 0;
-  int gold_sum = 0, check_sum = 0;
   int i,j;
   
   int offload_id_tmp, offload_id;
 
+  /* Stream-specific parameters. */
+  uint32_t src_V_width              = width;
+  uint32_t src_V_height             = height;
+  uint32_t src_V_stripe_height      = stripe_height;
+  uint32_t dst_V_width             = width;
+  uint32_t dst_V_height            = height;
+  uint32_t dst_V_stripe_height     = stripe_height;
+  
+  /* Dataset parameters. */
+  const unsigned src_V_stim_dim               = src_V_width * src_V_height;
+  const unsigned src_V_stripe_in_len          = src_V_width * src_V_stripe_height;
+  const unsigned dst_V_stim_dim              = dst_V_width * dst_V_height;
+  const unsigned dst_V_stripe_out_len        = dst_V_width * dst_V_stripe_height;
+
+  /* Address generator - Parameters for input streams */
+  // input src_V
+  const unsigned src_V_trans_size             = src_V_width * src_V_stripe_height;
+  const unsigned src_V_line_stride            = 0;
+  const unsigned src_V_line_length            = src_V_width * src_V_stripe_height;
+  const unsigned src_V_feat_stride            = 0;
+  const unsigned src_V_feat_length            = 1;
+  const unsigned src_V_feat_roll              = 0;
+  const unsigned src_V_loop_outer             = 0;
+  const unsigned src_V_realign_type           = 0;
+  const unsigned src_V_step                   = 4;
+  /* Address generator - Parameters for output streams */
+  // input src_V
+  const unsigned src_V_trans_size             = src_V_width * src_V_stripe_height;
+  const unsigned src_V_line_stride            = 0;
+  const unsigned src_V_line_length            = src_V_width * src_V_stripe_height;
+  const unsigned src_V_feat_stride            = 0;
+  const unsigned src_V_feat_length            = 1;
+  const unsigned src_V_feat_roll              = 0;
+  const unsigned src_V_loop_outer             = 0;
+  const unsigned src_V_realign_type           = 0;
+  const unsigned src_V_step                   = 4;
+
+  uint8_t *src_V_l1 = src_V;
+  uint8_t *dst_V_l1;
+
+  uint8_t *dst_V_golden_l1 = dst_V;
+
+  
   /* convolution-accumulation - HW */
 
   // enable hwpe
@@ -47,24 +98,51 @@ int main() {
 
   while((offload_id_tmp = hwpe_acquire_job()) < 0);
 
-  // set up bytecode
-  hwpe_bytecode_set(HWPE_LOOPS1_OFFS,           0x00000000);
-  hwpe_bytecode_set(HWPE_BYTECODE5_LOOPS0_OFFS, 0x00040000);
-  hwpe_bytecode_set(HWPE_BYTECODE4_OFFS,        0x00000000);
-  hwpe_bytecode_set(HWPE_BYTECODE3_OFFS,        0x00000000);
-  hwpe_bytecode_set(HWPE_BYTECODE2_OFFS,        0x00000000);
-  hwpe_bytecode_set(HWPE_BYTECODE1_OFFS,        0x000008cd);
-  hwpe_bytecode_set(HWPE_BYTECODE0_OFFS,        0x11a12c05);
+  // // set up bytecode
+  // hwpe_bytecode_set(HWPE_LOOPS1_OFFS,           0x00000000);
+  // hwpe_bytecode_set(HWPE_BYTECODE5_LOOPS0_OFFS, 0x00040000);
+  // hwpe_bytecode_set(HWPE_BYTECODE4_OFFS,        0x00000000);
+  // hwpe_bytecode_set(HWPE_BYTECODE3_OFFS,        0x00000000);
+  // hwpe_bytecode_set(HWPE_BYTECODE2_OFFS,        0x00000000);
+  // hwpe_bytecode_set(HWPE_BYTECODE1_OFFS,        0x000008cd);
+  // hwpe_bytecode_set(HWPE_BYTECODE0_OFFS,        0x11a12c05);
   
   // job-dependent registers
-  hwpe_a_addr_set((unsigned int) a);
-  hwpe_b_addr_set((unsigned int) b);
-  hwpe_c_addr_set((unsigned int) c);
-  hwpe_d_addr_set((unsigned int) d);
-  hwpe_nb_iter_set(4);
-  hwpe_len_iter_set(32-1);
-  hwpe_vectstride_set(32*4);
-  hwpe_shift_simplemul_set(hwpe_shift_simplemul_value(0, 0));
+  hwpe_src_V_addr_set( src_V_l1 );
+  hwpe_dst_V_addr_set( dst_V_l1 );
+
+  hwpe_width_set( width_val );
+  hwpe_height_set( height_val );
+
+  // hwpe_nb_iter_set(4);
+  hwpe_len_iter_set_dst_V(engine_runs_dst_V-1);
+
+  hwpe_addr_gen_src_V(
+    src_V_trans_size,
+    src_V_line_stride,
+    src_V_line_length,
+    src_V_feat_stride,
+    src_V_feat_length,
+    src_V_feat_roll,
+    src_V_loop_outer,
+    src_V_realign_type,
+    src_V_step
+  );
+
+  hwpe_addr_gen_dst_V(
+    dst_V_trans_size,
+    dst_V_line_stride,
+    dst_V_line_length,
+    dst_V_feat_stride,
+    dst_V_feat_length,
+    dst_V_feat_roll,
+    dst_V_loop_outer,
+    dst_V_realign_type,
+    dst_V_step
+  );
+
+  // hwpe_vectstride_set(32*4);
+  // hwpe_shift_simplemul_set(hwpe_shift_simplemul_value(0, 0));
 
   // start hwpe operation
   hwpe_trigger_job();
@@ -73,13 +151,14 @@ int main() {
   asm volatile ("wfi" ::: "memory");
 
   // disable hwpe
+  hwpe_soft_clear();
   hwpe_cg_disable();
   
   // check
-  if(((uint32_t *) d)[0] != 0x7f228fd6) errors++;
-  if(((uint32_t *) d)[1] != 0x23a7d5c2) errors++;
-  if(((uint32_t *) d)[2] != 0x7f281848) errors++;
-  if(((uint32_t *) d)[3] != 0x6127d834) errors++;
+  // if(((uint32_t *) d)[0] != 0x7f228fd6) errors++;
+  // if(((uint32_t *) d)[1] != 0x23a7d5c2) errors++;
+  // if(((uint32_t *) d)[2] != 0x7f281848) errors++;
+  // if(((uint32_t *) d)[3] != 0x6127d834) errors++;
 
   // return errors
   *(int *) 0x80000000 = errors;
